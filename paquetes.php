1e73984c1a0b40ad075dee0d3c7227be1498138a
JesFernandoGL <jesale.07@gmail.com>
2021-05-06 12:32:38 -0500

    060521_1

---
+++ paquetes.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Paquetes Inet Alto rendimiento para ti</title>
+</head>
+<body>
+    <?php include_once "includes/header.php" ?>
+    <div class="hero-paginas bg-paquetes"><!-- Seccion hero -->
+    <?php $titulo = "Conoce nuestros paquetes"?>
+    <?php $descripcion = "Pensados en obtener el mejor rendimiento"?>
+    <?php include_once "includes/hero.php" ?>
+    <section class="seccion bg-blanco"><!-- Seccion Paquetes -->
+        <div class="contenedor">
+            <div class="pg-paquetes">
+                <div class="contenedor-paquete w-3">
+                    <div class="paquete"><!-- Paquete Basic -->
+                        <div class="cintillo bg-paquete1">
+                            <div class="nombre-paquete">Basic</div>
+                            <div class="numero-paquete">5 MB</div>
+                        </div>
+                        <div class="contenido-paquete">
+                            <p class="descripcion-paquete">Ideal para:</p>
+                            <ul>
+                                <li>Redes sociales.</li>
+                                <li>Clases en línea. </li>
+                                <li>Entretenimiento.</li>
+                                <li>Venta en línea.</li>
+                                <li>Home office.</li>
+                            </ul>
+                            <p class="precio-paquete">$399</p>
+                            <a class="btn" href="">Contáctanos</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="contenedor-paquete w-3">
+                    <div class="paquete"><!-- Paquete Medium -->
+                        <div class="cintillo bg-paquete2">
+                            <div class="nombre-paquete">Medium</div>
+                            <div class="numero-paquete">7 MB</div>
+                        </div>
+                        <div class="contenido-paquete">
+                            <p class="descripcion-paquete">Ideal para:</p>
+                            <ul>
+                                <li>Redes sociales.</li>
+                                <li>Clases en línea. </li>
+                                <li>Entretenimiento.</li>
+                                <li>Venta en línea.</li>
+                                <li>Home office.</li>
+                            </ul>
+                            <p class="precio-paquete">$399</p>
+                            <a class="btn" href="">Contáctanos</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="contenedor-paquete w-3">
+                    <div class="paquete"><!-- Paquete Premium -->
+                        <div class="cintillo bg-paquete3">
+                            <div class="nombre-paquete">Premium</div>
+                            <div class="numero-paquete">10 MB</div>
+                        </div>
+                        <div class="contenido-paquete">
+                            <p class="descripcion-paquete">Ideal para:</p>
+                            <ul>
+                                <li>Redes sociales.</li>
+                                <li>Clases en línea. </li>
+                                <li>Entretenimiento.</li>
+                                <li>Venta en línea.</li>
+                                <li>Home office.</li>
+                            </ul>
+                            <p class="precio-paquete">$399</p>
+                            <a class="btn" href="">Contáctanos</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="contenedor-paquete w-1">
+                    <div class="paquete"><!-- Paquete Custom -->
+                        <div class="cintillo bg-paquete4">
+                            <div class="nombre-paquete">Custom</div>
+                            <div class="numero-paquete">Desde $399</div>
+                        </div>
+                        <div class="contenido-paquete">
+                            <p class="descripcion-paquete">Ideal para:</p>
+                            <ul>
+                                <li>Mb de subida.</li>
+                                <li>Tiempo de contrato.</li>
+                                <li>Plan + Básicos.</li>
+                                <li>Usos del internet( Subir Videos, profesión, videovigilancia,videojuegos)</li>
+                                <li>Instalación inmediata mismo día.</li>
+                                <li>Router mayor cobertura.</li>
+                            </ul>
+                            <p class="precio-paquete">$399</p>
+                            <a class="btn" href="">Contáctanos</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    
+    <?php include_once "includes/footer.php"?>
+
+
+    <script src="js/menu.js"></script>
+</body>
+</html>
